5471a953bdde09897b59ea29d948a9b869b35637
Kingller Tang <kingller.tang@gaiaworks.cn>
2023-08-10 16:31:27 +0800

    initialization

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # css-variables-to-less-loader
 A loader for webpack. Compiles CSS variables to less variables for support IE.
+
+## Install
+
+```bash
+npm install css-variables-to-less-loader
+``` 
+
+## Usage
+
+Add the loader to your `webpack` config. For example:
+
+<strong>webpack.config.js</strong>
+
+```
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.less$/i,
+        use: [
+          // compiles Less to CSS
+          "style-loader",
+          "css-loader",
+          "less-loader",
+          {
+            loader: 'css-variables-to-less-loader',
+            options: {
+                // the file include the less variables you want to replace the CSS variables
+                // for example, replace var(--primary) with @primary, if the file contains @primary variable
+                varFile: path.join(__dirname, 'src/css/color-variables.less'),
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+```
---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const lessToJs = require('less-vars-to-js');
+
+module.exports = async function (source) {
+    const callback = this.async();
+
+    const lessVarFilePath = this.query.varFile; // 通过options传入的Less变量文件路径
+
+    try {
+        if (!source || !/var\(--(.*?)\)/.test(source)) {
+            callback(null, source);
+            return;
+        }
+
+        // 读取Less变量文件内容
+        const lessVarContent = fs.readFileSync(lessVarFilePath, 'utf-8');
+
+        // 获取所有导入的变量
+        const importedVariables = lessToJs(lessVarContent);
+
+        // 替换CSS中的变量
+        const cssWithLessVariables = source.replace(/var\(--(.*?)\)/g, (_, variableName) => {
+            const lessVarName = `@${variableName.trim()}`;
+            return importedVariables[lessVarName] ? lessVarName : _;
+        });
+        callback(
+            null,
+            `@import '${lessFilePath}';\n${cssWithLessVariables}`
+        );
+    } catch (error) {
+        callback(error);
+    }
+};
